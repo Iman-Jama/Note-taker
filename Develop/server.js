9242b27f61a43943a3e1719de23acdf81c3a6804
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-04-16 21:51:53 +0100

    Updated server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,101 @@
 const express = require('express');
-
 const app = express();
 const PORT = process.env.port || 3001;
 const path = require('path');
+const uuid = require('./helpers/uuid')
+const notes = require('./db/db.json');
+const fs = require('fs');
 
-// Serve images, css files, js files from the public directory
-// Allows us to reference files with their relative path
-// Example: http://localhost:3001/images/gh.png
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Add a static middleware for serving assets in the public folder
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
 app.get('/notes', (req, res)=> res.sendFile(path.join(__dirname, '/public', 'notes.html')));
+
+
+app.get('/api/notes', (req, res) => {
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get notes`);
+
+  // Read the notes from the db.json file
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json('Error in getting notes');
+    } else {
+      // Parse the data from the file into a JavaScript array
+      const notes = JSON.parse(data);
+      
+      // Sending all notes to the client
+      res.json(notes);
+    }
+  });
+});
+
+
+  app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNotes = {
+        title,
+        text,
+        id: uuid(),
+      };
+
+      const response = {
+        status: 'success',
+        body: newNotes,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting notes');
+    }
+    
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+        const newNotes = {
+          title:req.body.title,
+          text: req.body.text,
+          id: uuid(),
+         };
+     
+        // Add a new review
+        parsedNotes.push(newNotes);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated Notes!')
+        );
+      }
+    });
+
+});
+
+app.delete('/api/notes/:note_id', (req, res)=>
+
+)
+
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes at http://localhost:${PORT}!`)
